191531d52a6d20e622e678192acd933ed485d3c4
ss2261 <ss2261@kent.ac.uk>
2021-04-27 15:38:09 +0100

    iterative selection of threshold algorithm - Inital commit

---
+++ src/iteractiveSelectionOfThreshold.java
@@ -0,0 +1,46 @@
+public class iteractiveSelectionOfThreshold {
+    public static void main(String[] args) {
+        int[] greyLevels = new int[16];
+        greyLevels[0] = 0;
+        greyLevels[1] = 1;
+        greyLevels[2] = 2;
+        greyLevels[3] = 3;
+        greyLevels[4] = 4;
+        greyLevels[5] = 5;
+        greyLevels[6] = 6;
+        greyLevels[7] = 7;
+        greyLevels[8] = 8;
+        greyLevels[9] = 9;
+        greyLevels[10] = 10;
+        greyLevels[11] = 11;
+        greyLevels[12] = 12;
+        greyLevels[13] = 13;
+        greyLevels[14] = 14;
+        greyLevels[15] = 15;
+
+        int[] frequency = new int[16];
+        frequency[0] = 0;
+        frequency[1] = 1;
+        frequency[2] = 0;
+        frequency[3] = 101;
+        frequency[4] = 380;
+        frequency[5] = 11;
+        frequency[6] = 6;
+        frequency[7] = 9;
+        frequency[8] = 11;
+        frequency[9] = 22;
+        frequency[10] = 57;
+        frequency[11] = 71;
+        frequency[12] = 30;
+        frequency[13] = 17;
+        frequency[14] = 8;
+        frequency[15] = 3;
+
+
+
+        alg();
+    }
+
+    public static void alg(){
+    }
+}
