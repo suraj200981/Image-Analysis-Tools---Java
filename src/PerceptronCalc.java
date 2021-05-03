e35af6bc7bb5f3a745f4ebfc5037eb9a20529053
ss2261 <ss2261@kent.ac.uk>
2021-05-03 17:56:18 +0100

    Perceptron neural network - essential parameters being passed

--- src/PerceptronCalc.java
+++ src/PerceptronCalc.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
+
 public class PerceptronCalc {
     public static void main(String[] args) {
 
-        alg();
+        ArrayList<Integer> patterns = new ArrayList<Integer>();
+        patterns.add(001);
+        patterns.add(110);
+
+
+        alg(patterns, 15,20,15,22,5);
 
     }
 
 
-    public static void alg(){
+    public static void alg(ArrayList<Integer> patterns, int w1, int w2, int w3, int threshold, int learningRate) {
 
         System.out.println("test");
     }
