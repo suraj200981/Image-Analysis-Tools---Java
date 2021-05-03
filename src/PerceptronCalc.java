a1c6aa12f3f4dde6f20dd2329e08a505c3431815
ss2261 <ss2261@kent.ac.uk>
2021-05-03 18:33:01 +0100

    Perceptron neural network - summation of weight correct for pattern 1 epoch 1

--- src/PerceptronCalc.java
+++ src/PerceptronCalc.java
@@ -3,18 +3,48 @@ import java.util.ArrayList;
 public class PerceptronCalc {
     public static void main(String[] args) {
 
-        ArrayList<Integer> patterns = new ArrayList<Integer>();
-        patterns.add(001);
-        patterns.add(110);
+        ArrayList<String> patterns = new ArrayList<String>();
+        patterns.add("001");
+        patterns.add("110");
 
 
-        alg(patterns, 15,20,15,22,5);
+        alg(patterns, 15, 20, 15, 22, 5);
 
     }
 
 
-    public static void alg(ArrayList<Integer> patterns, int w1, int w2, int w3, int threshold, int learningRate) {
+    public static void alg(ArrayList<String> patterns, int w1, int w2, int w3, int threshold, int learningRate) {
 
-        System.out.println("test");
+        System.out.println(getSummation(patterns, w1, w2, w3));
+
     }
+
+    public static int newWeightForumla(int t, int o, int n) {
+        int firstPart = t - o;
+        int secondPart = firstPart;
+        return (t - o);
+    }
+
+    public static int getSummation(ArrayList<String> patterns, int w1, int w2, int w3) {
+        ArrayList<Integer> weightsList = new ArrayList<Integer>();
+        weightsList.add(w1);
+        weightsList.add(w2);
+        weightsList.add(w3);
+
+        String s1 = patterns.get(0);
+        String s2 = patterns.get(1);
+
+        int sum = 0;
+
+        for (int x = 0; x<s1.length(); x++) {
+            char tempChar = s1.charAt(x);
+            int tempInt=Integer.parseInt(String.valueOf(tempChar));
+
+            sum+= weightsList.get(x)*tempInt;
+        }
+        return sum;
+
+    }
+
 }
+
