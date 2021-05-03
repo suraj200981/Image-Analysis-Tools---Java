25bc3f664bc1347024e45d19d621110fbd8ab94e
ss2261 <ss2261@kent.ac.uk>
2021-05-03 17:45:07 +0100

    Perceptron neural network - inital commit

---
+++ src/PerceptronCalc.java
@@ -0,0 +1,13 @@
+public class PerceptronCalc {
+    public static void main(String[] args) {
+
+        alg();
+
+    }
+
+
+    public static void alg(){
+
+        System.out.println("test");
+    }
+}
